358ba3e8e0f80cf3e372f751735269acf57ae03e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-10 23:45:07 -0300

    added first lecture code

--- course1/Fundamentos/src/Main.java
+++ course1/Fundamentos/src/Main.java
@@ -1,5 +1,22 @@
-public class App {
+import java.util.Scanner;
+
+public class Main {
+
+    // constant
+    private final static String WELCOME_MESSAGE = "Hallo";
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // my comment
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(WELCOME_MESSAGE);
+
+        System.out.print("Hallo, what's your name");
+        String name = scanner.nextLine();
+
+        System.out.print("Hallo, what's your name");
+        int age = scanner.nextInt();
+
+        System.out.println("Hallo " + name + " your  age is " + age);
+        System.out.printf("Hallo %s your age is %s \n", name, age);
     }
 }
