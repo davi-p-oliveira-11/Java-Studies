caeb013a0d4a0bb7d15514abe74a08f0712197b4
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-17 16:31:36 -0300

    added .gitignore file

--- course1/Fundamentos/src/For.java
+++ course1/Fundamentos/src/For.java
@@ -1,3 +1,12 @@
-public class For {
+import java.util.Scanner;
 
-}
+public class For {
+  
+   public static void main(String[] args) {
+     
+      for (var i = 1; i <= 100; i++) {
+         System.out.println(i);
+       }
+     }
+   }
+
