b478eb6bd2889c09e2f050fbd219e96556fe6c25
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-17 15:06:15 -0300

    updated switch case1

--- course1/Fundamentos/src/SwitchCase.java
+++ course1/Fundamentos/src/SwitchCase.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class SwitchCase {
+
+   public static void main(String[] args) {
+      var scanner = new Scanner(System.in);
+      System.out.println("Enter a Numberr starting 1 to 7");
+      var option = scanner.nextInt();
+      switch (option) {
+        case 1:
+        case 7:
+          System.out.println("Weekend !");
+          break;
+        case 2:
+          System.out.println("Monday");
+          break;
+        case 3:
+          System.out.println("Tuesday");
+          break;
+        case 4:
+          System.out.println("Wednesday");
+          break;
+        case 5:
+          System.out.println("Thrusday");
+          break;
+        case 6:
+          System.out.println("Friday");
+          break;
+        default:
+          System.err.println("Invalid option");
+      }
+   }
 
 }
