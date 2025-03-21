3daf973a5ab1e28d227524fbecfcbac3802f50b5
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-20 22:36:49 -0300

    updated for

--- course1/Fundamentos/src/For.java
+++ course1/Fundamentos/src/For.java
@@ -5,6 +5,10 @@ public class For {
    public static void main(String[] args) {
      
       for (var i = 1; i <= 100; i++) {
+        if (i % 2 == 0) {
+           continue;
+        }
+
          System.out.println(i);
        }
      }
